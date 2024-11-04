04181ffeb020d42bb183c2e58081b743c65cfcd5
Varun Karli <varunkarli2004@gmail.com>
2024-11-04 12:40:12 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -35,7 +35,14 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n;
+    printf("size:");
+    scanf("%d", &n);
+    int arr[n];
+    printf("data:");
+    for(int i=0; i<n; i++){
+      scanf("%d",&arr[i]);
+    }
     int arr_size = sizeof(arr) / sizeof(arr[0]);
 
     printf("Given array is \n");
